b856fb22800acefa3faf62c07ff1eb57ce59010f
LisaNaccache <naccachelisa@gmail.com>
2024-11-18 10:46:50 +0100

    Add todo-list-page

---
+++ frontend/src/app/todo-list-page/todo-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list-page.component.html',
+  styleUrl: './todo-list-page.component.css'
+})
+export class TodoListPageComponent {
+
+}
