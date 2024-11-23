0cc9763e91c4dd9fc1208b5eac2fd9b55c405f3a
LisaNaccache <naccachelisa@gmail.com>
2024-11-23 23:36:44 +0100

    Update app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, provideZoneChangeDetection, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {ApiModule, Configuration, ConfigurationParameters} from '../generated/angular-client';
+
+const apiConfParams: ConfigurationParameters = {basePath: 'http://localhost:4200/'} // override generated code }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideHttpClient(), importProvidersFrom(ApiModule.forRoot(() => new Configuration(apiConfParams)))]
 };
