a61777479ad72cda8afb54a78f0ba1df22db08b3
LisaNaccache <naccachelisa@gmail.com>
2024-11-23 23:21:43 +0100

    Add directory genered with swagger-codegen-cli

---
+++ frontend/src/generated/angular-client/model/models.ts
@@ -0,0 +1,7 @@
+export * from './learningPackage';
+export * from './learningPackageNoId';
+export * from './todo';
+export * from './todoNoId';
+export * from './userLearningFact';
+export * from './userLearningFactNoId';
+export * from './userPackageLearning';
