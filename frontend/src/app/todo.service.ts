89c158e62cb9e2d9515826d64afe7f16ef5ebe86
LisaNaccache <naccachelisa@gmail.com>
2024-11-23 22:16:47 +0100

    Add + Update : Refactor the code to return Model from the Service

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
 import {TodoDTO} from './interfaces/todo-dto';
+import {TodoModel} from '../Models/todo.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,18 @@ export class TodoService {
 
   private readonly apiBaseUrl = 'http://localhost:3000/api';
 
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(private readonly httpClient: HttpClient) {
+  }
 
+  /*
+     pipe : sert a transformer ou manipuler les donnees d'un observable
+     map : permet de prendre chaque element dans l observable et la modifier
+     x => new TodoModel(x) : fonction anonyme transforme chaque élément x (de type TodoDTO) du tableau en un nouvel objet TodoModel
+     Convertir des données brutes (TodoDTO[]) en un format plus utile (TodoModel[]).
+   */
   getAllTodoDTOs(): Observable<TodoDTO[]> {
-    return this.httpClient.get<TodoDTO[]>(`${this.apiBaseUrl}/todos`);
+    return this.httpClient.get<TodoDTO[]>(`${this.apiBaseUrl}/todos`)
+      .pipe(map(src => src.map(x => new TodoModel(x))));
   }
 
   /*addTodoDTO(todo: TodoDTO): Observable<TodoDTO> {
