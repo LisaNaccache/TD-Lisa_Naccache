70eb4d8a11a86bea8d7a23627f05ef1f7880e721
LisaNaccache <naccachelisa@gmail.com>
2024-11-25 23:09:06 +0100

    Update config and route (TD3)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import {ApplicationConfig, provideZoneChangeDetection, importProvidersFrom} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
 import {ApiModule, Configuration, ConfigurationParameters} from '../generated/angular-client';
 
-const apiConfParams: ConfigurationParameters = {basePath: 'http://localhost:4200/'} // override generated code }
+const apiConfParams: ConfigurationParameters = {basePath: 'http://localhost:4200'} // override generated code }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideHttpClient(), importProvidersFrom(ApiModule.forRoot(() => new Configuration(apiConfParams)))]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes, withHashLocation()), provideHttpClient(), importProvidersFrom(ApiModule.forRoot(() => new Configuration(apiConfParams)))]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import {Routes} from '@angular/router';
 import {TodoListPageComponent} from './todo-list-page/todo-list-page.component';
+import {LessonEditFormComponent} from './lesson-edit-form/lesson-edit-form.component';
+import {LessonListPageComponent} from './lesson-list-page/lesson-list-page.component';
+import {LessonSearchPageComponent} from './lesson-search-page/lesson-search-page.component';
+import {LessonDetailPageComponent} from './lesson-detail-page/lesson-detail-page.component';
+import {TestPage1Component} from './test-page1/test-page1.component';
 
 export const routes: Routes = [
   {path: 'todos', component: TodoListPageComponent},
+  {path: 'lesson-edit-form', component: LessonEditFormComponent},
+  {path: 'lesson-list', component: LessonListPageComponent},
+  {path: 'lesson-search', component: LessonSearchPageComponent},
+  {path: 'lesson/:id', component: LessonDetailPageComponent},
+  {path: 'test-page1', component: TestPage1Component}
 ];
