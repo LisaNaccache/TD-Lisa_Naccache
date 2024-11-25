cf37cc1cf75925fd4821c05ff4c75ccca908bff6
LisaNaccache <naccachelisa@gmail.com>
2024-11-25 09:17:00 +0100

    Create project td3

--- td3/src/app/app.component.spec.ts
+++ td3/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NodeReact_TP3'`, () => {
+  it(`should have the 'td3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NodeReact_TP3');
+    expect(app.title).toEqual('td3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NodeReact_TP3 app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, td3');
   });
 });
