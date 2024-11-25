6fa17b02c6a5991d0787332795d422c53dde1001
LisaNaccache <naccachelisa@gmail.com>
2024-11-25 08:47:05 +0100

    Upload TP3

---
+++ NodeReact_TP3/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'NodeReact_TP3';
+
+  constructor() {
+    console.log('AppComponent.constructor()')
+  }
+
+  ngOnInit(): void {
+    console.log('AppComponent.ngOnInit()')
+  }
+}
